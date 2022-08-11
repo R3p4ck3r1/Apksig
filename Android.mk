1eca55912be4361dd7ee471501d5d6b40eacf4f1
Edward Chiang <edwardhuan@google.com>
2022-08-11 17:39:31 +0000

    Add Android.mk
    Change-Id: I85b42bb6d57b050f8521635df9a3cdd54ba592bd

---
+++ Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+all_src_files := \
+  $(call all-java-files-under,src/main/java)
+
+# Platform apksig JNI library
+include $(CLEAR_VARS)
+LOCAL_MODULE := apksig
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(all_src_files)
+LOCAL_NO_STANDARD_LIBRARIES := true
+LOCAL_JAVA_LANGUAGE_VERSION := 1.8
+
+java9_or_greater := $(shell test $(javac_major_version) -ge 9 && echo true)
+ifeq ($(java9_or_greater),true)
+  # --add-exports flag is only allowed on java9 or greater.
+  # TODO(b/162131149) This is just a workaround. We should remove usage of
+  # this internal package and use bouncycastle instead.
+  LOCAL_JAVACFLAGS := --add-exports java.base/sun.security.pkcs=ALL-UNNAMED \
+    --add-exports java.base/sun.security.x509=ALL-UNNAMED
+endif
+include $(BUILD_HOST_JAVA_LIBRARY)
+
+all_src_files :=
